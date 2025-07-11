402e520b584dede904c73212eefe60c5dfc23a02
wokaikaixinxin <961211925@qq.com>
2025-07-11 10:33:09 +0800

    add SARDet_100K

--- ai4rs/evaluation/metrics/__init__.py
+++ ai4rs/evaluation/metrics/__init__.py
@@ -2,5 +2,7 @@
 from .dota_metric import DOTAMetric
 from .rotated_coco_metric import RotatedCocoMetric
 from .icdar2015_metric import ICDAR2015Metric
+from .coco_metric_sardet_100k import CocoMetricSARDet100k
 
-__all__ = ['DOTAMetric', 'RotatedCocoMetric', 'ICDAR2015Metric']
+__all__ = ['DOTAMetric', 'RotatedCocoMetric', 'ICDAR2015Metric',
+           'CocoMetricSARDet100k']
---
+++ projects/SARDet_100k/sardet_100k/__init__.py
@@ -0,0 +1,5 @@
+from .MSFA import MSFA
+
+__all__ = [
+    'MSFA'
+]
