a176b6327e1e5e68d70a3aaadbb14920be329c1e
wokaikaixinxin <961211925@qq.com>
2025-07-11 10:49:50 +0800

    update sardet100k

--- ai4rs/datasets/__init__.py
+++ ai4rs/datasets/__init__.py
@@ -3,11 +3,11 @@ from .dior import DIORDataset  # noqa: F401, F403
 from .dota import DOTAv2Dataset  # noqa: F401, F403
 from .dota import DOTADataset, DOTAv15Dataset
 from .hrsc import HRSCDataset  # noqa: F401, F403
-from .sardet_100k import SAR_Det_Finegrained_Dataset  # noqa: F401, F403
 from .icdar2015 import ICDAR15Dataset  # noqa: F401, F403
+from .sardet_100k import SAR_Det_Finegrained_Dataset  # noqa: F401, F403
 from .transforms import *  # noqa: F401, F403
 
 __all__ = [
     'DOTADataset', 'DOTAv15Dataset', 'DOTAv2Dataset', 'HRSCDataset',
-    'DIORDataset', 'SAR_Det_Finegrained_Dataset'
+    'DIORDataset', 'ICDAR15Dataset', 'SAR_Det_Finegrained_Dataset'
 ]
